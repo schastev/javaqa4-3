9d671dc59c66fcb56ad12a902f0545f03aec5414
k-emiko <62057364+k-emiko@users.noreply.github.com>
2021-03-14 01:21:32 +1100

    modified Main now that I've actually read the specs

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int creditsum = 1_000_000;
-        int duration = 1;
         double annualrate = 9.99;
         CreditPaymentService service = new CreditPaymentService();
-        int monthlypayment = (int)service.calculate(creditsum, duration * 12, annualrate); // duration * 12 bc we need n to be in months
-        System.out.println(monthlypayment);
+        int monthlypayment = (int)service.calculate(creditsum, 1 * 12, annualrate); // duration * 12 bc we need n to be in months
+        System.out.println("monthly payment for the following parameters: 1M, 1 year, 9.99%: " + monthlypayment);
+        monthlypayment = (int)service.calculate(creditsum, 2 * 12, annualrate); // duration * 12 bc we need n to be in months
+        System.out.println("monthly payment for the following parameters: 1M, 2 years, 9.99%: " + monthlypayment);
+        monthlypayment = (int)service.calculate(creditsum, 3 * 12, annualrate); // duration * 12 bc we need n to be in months
+        System.out.println("monthly payment for the following parameters: 1M, 3 years, 9.99%: " + monthlypayment);
 
     }
 }
